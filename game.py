2d283d5899934e2aa1ad4bfe0361ee7944d8e46d
Arman Shaikh <www.armanas@gmail.com>
2024-11-08 20:38:58 -0500

    2nd commit

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 def main():
     print("Welcome to Rock, Paper, Scissors Game!")
+    print("Instructions: Choose 'rock', 'paper', or 'scissors'.")
+    print("The computer will also make a choice. See who wins!")
 
 if __name__ == "__main__":
     main()
